a80156422d940b9f0dfd50898e4bc6d223e58701
Steve <la14b@yahoo.co.uk>
2022-07-14 11:11:21 +0000

    Added requirements.txt and updated email

--- rus_energy/settings.py
+++ rus_energy/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT', False)
+DEBUG = os.environ.get('DEVELOPMENT', True)
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -149,7 +149,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.smtp.office365.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')

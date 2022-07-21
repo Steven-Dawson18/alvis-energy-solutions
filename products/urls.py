772aaf41b6906930be6f78d53492388bca379e1c
Steve <la14b@yahoo.co.uk>
2022-07-21 13:49:16 +0000

    Added product detail page

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name="products"),
+    path('ecoflo_smart/', views.ecoflo_smart, name="ecoflo_smart"),
 ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def products(request):
     '''Products page view'''
     return render(request, "products/products.html")
+
+
+def ecoflo_smart(request):
+    '''Ecoflo Smart page view'''
+    return render(request, "products/ecoflosmart.html")
